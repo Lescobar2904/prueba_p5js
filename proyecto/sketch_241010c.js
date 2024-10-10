dee35b7f2c72a1472dd5e005be472ed80aacac9d
Lukas Escobar <lescobar2023@alu.uct.cl>
2024-10-10 01:41:24 -0300

    cambio

--- proyecto/sketch_241010c.js
+++ proyecto/sketch_241010c.js
@@ -1,8 +1,35 @@
-function setup() {
+let images = [];
+let currentIndex = 0;
 
+function preload() {
+  // Carga tus imágenes aquí
+  for (let i = 1; i <= 5; i++) {
+    images.push(loadImage(`images/image${i}.jpg`)); // Asegúrate de que las imágenes estén en la carpeta 'images'
+  }
 }
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  displayImage();
+}
 
 function draw() {
+  // El dibujo se hace en la función displayImage()
+}
 
+function displayImage() {
+  background(255);
+  if (images.length > 0) {
+    image(images[currentIndex], 0, 0, width, height);
+  }
+}
+
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    currentIndex = (currentIndex + 1) % images.length; // Siguiente imagen
+    displayImage();
+  } else if (keyCode === LEFT_ARROW) {
+    currentIndex = (currentIndex - 1 + images.length) % images.length; // Imagen anterior
+    displayImage();
+  }
 }

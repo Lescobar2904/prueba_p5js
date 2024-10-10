c4c7924470c05301c98ee149c79665a9c1600e07
Lukas Escobar <lescobar2023@alu.uct.cl>
2024-10-10 02:14:55 -0300

    Update sketch_241010c.js

--- proyecto/sketch_241010c.js
+++ proyecto/sketch_241010c.js
@@ -1,18 +1,24 @@
 let img; // Variable para almacenar la imagen
 let currentImageIndex = 1; // Índice de la imagen actual
+let imgWidth = 640; // Ancho deseado para la imagen
+let imgHeight = 640; // Alto deseado para la imagen
 
 function preload() {
     img = loadImage(`images/image${currentImageIndex}.jpeg`); // Cargar la primera imagen
 }
 
 function setup() {
-    createCanvas(400, 400); // Tamaño del lienzo
+    createCanvas(windowWidth, windowHeight); // Tamaño del lienzo responsivo
 }
 
 function draw() {
-    background(255); // Fondo blanco
+    background(0); // Fondo negro
+    // Calcular el centro de la pantalla y asegurar que la imagen no abarque toda la ventana
+    let x = (width - imgWidth) / 2;
+    let y = (height - imgHeight) / 2;
+    
     // Mostrar la imagen en el centro de la pantalla
-    image(img, (width - 640) / 2, (height - 640) / 2, 440, 440); // Escala a 640x640
+    image(img, x, y, imgWidth, imgHeight); // Mostrar la imagen en tamaño deseado
 }
 
 function mousePressed() {
@@ -21,4 +27,8 @@ function mousePressed() {
         currentImageIndex = 1;
     }
     img = loadImage(`images/image${currentImageIndex}.jpeg`); // Cargar la nueva imagen
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight); // Ajustar el lienzo al cambiar el tamaño de la ventana
 }

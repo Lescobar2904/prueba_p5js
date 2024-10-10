220fa7ce4807919d0013a3dc569f819dfbaaff37
Lukas Escobar <lescobar2023@alu.uct.cl>
2024-10-10 02:08:22 -0300

    Update sketch_241010c.js

--- proyecto/sketch_241010c.js
+++ proyecto/sketch_241010c.js
@@ -1,63 +1,24 @@
-let images = [];
-let currentIndex = 0;
-const maxWidth = 800;  // Ancho máximo para las imágenes
-const maxHeight = 800; // Alto máximo para las imágenes
+let img; // Variable para almacenar la imagen
+let currentImageIndex = 1; // Índice de la imagen actual
 
 function preload() {
-  // Carga las imágenes desde image1.jpeg hasta image9.jpeg
-  for (let i = 1; i <= 9; i++) {
-    images.push(loadImage(`images/image${i}.jpeg`));
-  }
+    img = loadImage(`images/image${currentImageIndex}.jpeg`); // Cargar la primera imagen
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  displayImage();
+    createCanvas(800, 800); // Tamaño del lienzo
 }
 
 function draw() {
-  // El dibujo se hace en la función displayImage()
-}
-
-function displayImage() {
-  background(255);
-  if (images.length > 0) {
-    let img = images[currentIndex];
-    let aspectRatio = img.width / img.height;
-
-    // Calcular el tamaño de la imagen manteniendo la relación de aspecto
-    let imgWidth, imgHeight;
-
-    if (img.width > img.height) {
-      imgWidth = maxWidth; // Limitar ancho
-      imgHeight = maxWidth / aspectRatio; // Mantener relación de aspecto
-    } else {
-      imgHeight = maxHeight; // Limitar alto
-      imgWidth = maxHeight * aspectRatio; // Mantener relación de aspecto
-    }
-
-    // Centrar la imagen en el lienzo
-    image(img, (width - imgWidth) / 2, (height - imgHeight) / 2, imgWidth, imgHeight);
-  }
-}
-
-function keyPressed() {
-  if (keyCode === RIGHT_ARROW) {
-    currentIndex = (currentIndex + 1) % images.length; // Siguiente imagen
-    displayImage();
-  } else if (keyCode === LEFT_ARROW) {
-    currentIndex = (currentIndex - 1 + images.length) % images.length; // Imagen anterior
-    displayImage();
-  }
+    background(255); // Fondo blanco
+    // Mostrar la imagen en el centro de la pantalla
+    image(img, (width - 640) / 2, (height - 640) / 2, 640, 640); // Escala a 640x640
 }
 
 function mousePressed() {
-  // Cambia a la siguiente imagen al hacer clic
-  currentIndex = (currentIndex + 1) % images.length; // Siguiente imagen
-  displayImage();
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  displayImage();
+    currentImageIndex++; // Incrementar el índice
+    if (currentImageIndex > 9) { // Reiniciar si llega al final
+        currentImageIndex = 1;
+    }
+    img = loadImage(`images/image${currentImageIndex}.jpeg`); // Cargar la nueva imagen
 }
